46c17cd1c2c9761b160af6faa9f3ba6de95c4571
sharunkuhar <sharun.kuhar62@gmail.com>
2026-04-25 20:08:03 +0000

    Added content

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,8 +4,8 @@ import rss from '@astrojs/rss';
 export async function GET(context) {
 	const posts = await getCollection('blog');
 	return rss({
-		title: 'My Astro Blog',
-		description: 'The perfect starter for your perfect blog.',
+		title: 'Sharun Kuhar',
+		description: 'My personal website.',
 		site: context.site,
 		items: posts.map((post) => ({
 			title: post.data.title,
