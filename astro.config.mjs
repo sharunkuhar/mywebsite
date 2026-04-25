0252b7b0df3bffb578e790ce3ebb9dd1e5fc2a09
sharunkuhar <sharun.kuhar62@gmail.com>
2026-04-25 16:30:15 +0000

    Deployed via github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://sharunkuhar.github.io',
+	base: '/mywebsite',
 	server: { host: true },
 	integrations: [mdx(), sitemap()],
 	fonts: [
